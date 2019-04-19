ab064326ec99fcf1b6e0978853d22bf7e0cbb7f7
diogosbr <diogosbr@gmail.com>
2019-04-19 14:46:35 -0300

    modificando o layout
    Fiz diversas modificações no layout.
    Criei abas para as funções.
    Passei a usar fluidRow

--- ui.R
+++ ui.R
@@ -1,94 +1,132 @@
-ui <- fluidPage(
-  useShinyalert(),
-  
-  # Título do app. ####
-  titlePanel("RGIS"),
-  
-  # Barra lateral com as definições do input e do output. ####
-  sidebarLayout(
-    
-    # Barra lateral para os inputs. ####
-    sidebarPanel(width = 3,
+#Titulo ----
+header <- dashboardHeader(title = "RGIS")
 
-    #inserindo arquivo csv ####
-    fileInput(
-        "file1",
-        "Tabela (.csv)",
-        multiple = FALSE,
-        accept = c("text/csv",
-                   "text/comma-separated-values,text/plain",
-                   ".csv"),
-        width = "100%",
-        buttonLabel = "Arquivo",
-        placeholder = "Insira arquivo csv"
-      ),
-    
-    textInput(inputId="lon", label = "lon", value = "lon", width = "25%"),
-    textInput(inputId="lat", label = "lat", value = "lat", width = "25%"),
-      # Input: Checkbox if file has header ####
-      checkboxInput("header", "Cabeçalho", TRUE),
-      
-      # Input: Select separator ####
-      radioButtons(
-        "sep",
-        "Separador de coluna",
-        choices = c(
-          " ; " = ";",
-          " , " = ",",
-          "tab" = "\t"
+#corpo ----
+body <- dashboardBody(
+  fluidRow(
+  useShinyalert(),
+  #barra lateral (abas) ----
+  column(width = 3,
+    #config. abas ----
+    tabsetPanel(
+      #CSV (aba) ----
+      tabPanel(
+        "CSV",
+        #inserir arquivo ----
+        fileInput(
+          "file1",
+          "Selecionar arquivo",
+          multiple = FALSE,
+          accept = c("text/csv",
+                     "text/comma-separated-values,text/plain",
+                     ".csv"),
+          #width = "100%",
+          buttonLabel = "Arquivo",
+          placeholder = "Insira arquivo csv"
         ),
-        inline = T,
-        selected = ","
+        #nome coluna longitude ----
+        textInput(
+          inputId = "lon",
+          label = "lon",
+          value = "lon",
+          width = "25%"
+        ),
+        #nome coluna longitude ----
+        textInput(
+          inputId = "lat",
+          label = "lat",
+          value = "lat",
+          width = "25%"
+        ),
+        
+        #Checkbox cabeçalho ----
+        checkboxInput("header", "Cabeçalho", TRUE),
+        
+        #Separador coluna ----
+        radioButtons(
+          "sep",
+          "Separador de coluna",
+          choices = c(" ; " = ";",
+                      " , " = ",",
+                      "tab" = "\t"),
+          inline = T,
+          selected = ","
+        ),
+        #Botao OK ----
+        actionButton("plot_point", "OK")
       ),
-      
-      actionButton("plot_point", "OK"),
-      
-      # Horizontal line
-      tags$hr(), 
-      
-      # Main panel for displaying outputs
-      tabPanel("Head da tabela",tableOutput("df")),
-    
-      # Horizontal line
-      tags$hr(),
-      
-      #inserindo arquivo shape ####
-      textInput("shape_path", "Shape path", "./Exemplos/biomas.shp"),
-      #fileInput("file3", "Vetor", multiple = TRUE, accept = c('.shp','.dbf','.sbn','.sbx','.shx','.prj')),
-      selectInput("cor", "Cor", choices = c('black', "green", "blue", "red", "white", "darkgreen", "darkblue", "darkred"), width = "50%"),
-      actionButton("plot_shape", "OK"),
-      
-      # Horizontal line
-      tags$hr(),
-      
-      #inserindo arquivo raster ####
-      fileInput(
-        "file2",
+      #Raster (aba)----
+      tabPanel(
         "Raster",
-        multiple = FALSE,
-        accept = c("image/tiff",
-                   ".tiff",
-                   ".asc",
-                   ".bil"),
-        buttonLabel = "Arquivo",
-        placeholder = "Insira o raster"
+        #inserir arquivo ----
+        fileInput(
+          "file2",
+          "Selecionar arquivo",
+          multiple = FALSE,
+          accept = c("image/tiff",
+                     ".tiff",
+                     ".asc",
+                     ".bil"),
+          buttonLabel = "Arquivo",
+          placeholder = "Insira o raster"
+        ),
+        #Botao OK e ADD
+        actionButton("plot_raster", "OK"),
+        actionButton("add_point", "Add"),
+        
+        #barra da tranparencia ----
+        sliderInput(
+          inputId = "alpha",
+          label = "Opacidade",
+          min = 0,
+          max = 1,
+          value = 0.5
+        )
       ),
-      actionButton("plot_raster", "OK"),
-      actionButton("add_point", "Add"),
-      
-      #barra da tranparencia ####
-      sliderInput(inputId = "alpha",
-                  label = "Transparencia do raster",
-                  min = 0,
-                  max = 1,
-                  value = 0.5)
-    ),
-    
-    # Painel principal para mostrar os outputs.####
-    mainPanel(
-      h3("Mapa"),
-      # Output: MAPA
-      leafletOutput("mymap",height = 800)
+        #Vetor (aba) ----
+      tabPanel(
+        "Vetor",
+        #inserir arquivo
+        textInput("shape_path", "Selecionar caminho do arquivo", "./Exemplos/biomas.shp"),
+        #fileInput("file3", "Vetor", multiple = TRUE, accept = c('.shp','.dbf','.sbn','.sbx','.shx','.prj')),
+        
+        #cor da linha ----
+        selectInput(
+          "cor",
+          "Cor",
+          choices = c(
+            'black',
+            "green",
+            "blue",
+            "red",
+            "white",
+            "darkgreen",
+            "darkblue",
+            "darkred"
+          ),
+          width = "50%"
+        ),
+        #Botao OK
+        actionButton("plot_shape", "OK")
+      ),
+      #ADD (aba) ----
+      tabPanel("ADD", 
+               h3("Em construção...")),
+      #FUN (aba) ----
+      tabPanel("FUN",
+               h3("Em construção...")),
+      #Help (aba) ----
+      tabPanel("help", 
+               h3("Em construção..."))
     )
-  )
-)  
+  ),
+  #MAPA ----
+  mainPanel(h3("Mapa"),
+            # Output: MAPA
+            leafletOutput("mymap", height = 800))
+))
+
+#?
+dashboardPage(header,
+              dashboardSidebar(disable = TRUE),
+              body)
